73e430b78749fd1b38b7852f200fc87386e8aed0
dhrubo-10 <sdhrubo770@gmail.com>
2025-11-05 13:42:40 +0600

    updated fs core

--- f_sys/vfs.c
+++ f_sys/vfs.c
@@ -1,26 +1,35 @@
+#include <kernel_b/types.h>
+#include <kernel_b/string.h>
+#include <kernel_b/stdlib.h>
 #include "fs.h"
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-file_t* root = NULL;
-file_t* current_dir = NULL;
+static file_t* root = NULL;
+static file_t* current_dir = NULL;
 
 static file_t* create_file(const char* name, file_type_t type, file_t* parent) {
-    file_t* f = malloc(sizeof(file_t));
+    file_t* f = kmalloc(sizeof(file_t));
+    if (!f)
+        return NULL;
+
     memset(f, 0, sizeof(file_t));
     strncpy(f->name, name, MAX_NAME_LEN);
     f->type = type;
     f->parent = parent;
+    f->child_count = 0;
     return f;
 }
 
-void fs_init() {
+void fs_init(void) {
     root = create_file("/", FILE_TYPE_DIR, NULL);
+    if (!root)
+        panic("fs_init: failed to allocate root directory");
     current_dir = root;
 }
 
 file_t* fs_find_in_dir(file_t* dir, const char* name) {
+    if (!dir || dir->type != FILE_TYPE_DIR)
+        return NULL;
+
     for (int i = 0; i < dir->child_count; i++) {
         if (strcmp(dir->children[i]->name, name) == 0)
             return dir->children[i];
