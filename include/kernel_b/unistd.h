181a99142e4136b7da63527e7920bade6c5889ad
dhrubo-1 <sdhrubo770@gmail.com>
2026-01-31 00:34:14 +0600

    added: unistd.h

---
+++ include/kernel_b/unistd.h
@@ -0,0 +1,20 @@
+#ifndef _UNISTD_H
+#define _UNISTD_H
+
+#include "types.h"
+
+#define STDIN_FILENO  0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+
+pid_t getpid(void);
+pid_t getppid(void);
+uid_t getuid(void);
+gid_t getgid(void);
+int close(int fd);
+off_t lseek(int fd, off_t offset, int whence);
+int unlink(const char *pathname);
+void _exit(int status);
+char *getprogname(void);
+
+#endif
