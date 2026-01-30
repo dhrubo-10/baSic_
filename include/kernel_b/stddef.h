5cc7e26d72723d0c1d8af37c31d6e178d3ea362a
dhrubo-1 <sdhrubo770@gmail.com>
2026-01-31 00:32:51 +0600

    updaed & fixed: stddef.h

--- include/kernel_b/stddef.h
+++ include/kernel_b/stddef.h
@@ -1,9 +1,8 @@
-#ifndef KERNELB_STDDEF_H
-#define KERNELB_STDDEF_H
+#ifndef _STDDEF_H
+#define _STDDEF_H
 
 #include "types.h"
 
-#define offsetof(type, member) ((size_t)&(((type*)0)->member))
-#define NULL ((void*)0)
+#define offsetof(type, member) ((size_t)&((type*)0)->member)
 
 #endif
