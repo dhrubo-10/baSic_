63607b470a8631537ee5674eff25b15d7728a89c
dhrubo-1 <sdhrubo770@gmail.com>
2026-01-31 01:28:24 +0600

    kernel panic

--- include/kernel_b/panic.h
+++ include/kernel_b/panic.h
@@ -1,6 +1,13 @@
 #ifndef KERNELB_PANIC_H
 #define KERNELB_PANIC_H
 
-void panic(const char *msg);
+#include <kernel_b/stdio.h>
+
+#define panic(fmt, ...) do { \
+    printk("KERNEL PANIC at %s:%d: ", __FILE__, __LINE__); \
+    printk(fmt, ##__VA_ARGS__); \
+    printk("\n"); \
+    for(;;); \
+} while(0)
 
 #endif
