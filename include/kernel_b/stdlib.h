f47272504e5f7448630ec83ccc450e491d210f76
dhrubo-1 <sdhrubo770@gmail.com>
2026-01-31 00:37:50 +0600

    added: time.h

--- include/kernel_b/stdlib.h
+++ include/kernel_b/stdlib.h
@@ -1,10 +1,15 @@
-#ifndef KERNELB_STDLIB_H
-#define KERNELB_STDLIB_H
+#ifndef _STDLIB_H
+#define _STDLIB_H
 
 #include "types.h"
 
-void abort(void) __attribute__((noreturn));
-int atoi(const char *s);
-void itoa(int value, char *buf, int base);
+void *malloc(size_t size);
+void free(void *ptr);
+void *realloc(void *ptr, size_t size);
+void *calloc(size_t nmemb, size_t size);
+void abort(void);
+void exit(int status);
+int atoi(const char *nptr);
+long atol(const char *nptr);
 
 #endif
