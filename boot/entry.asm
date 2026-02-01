c51a12d71de2b71115679add198710a7705ede39
dhrubo-1 <sdhrubo770@gmail.com>
2026-02-02 02:39:44 +0600

    added: entry point

--- boot/entry.asm
+++ boot/entry.asm
@@ -1,26 +1,9 @@
 bits 32
 
-section .multiboot
-align 4
-multiboot_header:
-    dd 0x1BADB002          ; Magic
-    dd 0x03                ; Flags
-    dd -(0x1BADB002 + 0x03) ; Checksum
-
-section .text
 global _start
 _start:
     ; Setup stack
     mov esp, stack_top
-    
-    ; Clear BSS
-    extern bss_start
-    extern bss_end
-    mov edi, bss_start
-    mov ecx, bss_end
-    sub ecx, bss_start
-    xor eax, eax
-    rep stosb
     
     ; Call kernel main
     extern kernel_main
