c087f340b41cebe40b9f6bfa2e06780dab359f80
dhrubo-1 <sdhrubo770@gmail.com>
2026-02-02 02:40:26 +0600

    system headers

---
+++ include/kernelb/stdio.h
@@ -0,0 +1,6 @@
+#ifndef KERNELB_STDIO_H
+#define KERNELB_STDIO_H
+
+void printk(const char *str);
+
+#endif
---
+++ include/kernelb/types.h
@@ -0,0 +1,11 @@
+#ifndef KERNELB_TYPES_H
+#define KERNELB_TYPES_H
+
+typedef unsigned int uint32_t;
+typedef int int32_t;
+typedef unsigned short uint16_t;
+typedef short int16_t;
+typedef unsigned char uint8_t;
+typedef char int8_t;
+
+#endif
